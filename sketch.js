1e19ff93ee61cffef0bf9e974c822877cfa402a6
Kartikey <rawatkari554@gmail.com>
2020-12-21 22:50:52 +0530

    Aded the WebCam feature

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 //console.log('ml5 version:', ml5.version);
 let mobilenet;
-let dog;
+//let dog;
+let video;
+let label='';
+let prob;
 function modelReady(){
     console.log("model is ready");
-    mobilenet.predict(dog,gotResult);
+    mobilenet.predict(gotResult);
 }
 function gotResult(error,results){
     if(error){
@@ -11,18 +14,30 @@ function gotResult(error,results){
     }
     else{
         console.log(results);
+        label = results[0].label;
+        prob = results[0].confidence;
+        
+        mobilenet.predict(gotResult);
     }
+    
 
 }
-function imageReady(){
-    image(dog, 0 , 0 , width , height);
-}
+// function imageReady(){
+//     image(dog, 0 , 0 , width , height);
+// }
 function setup(){
     createCanvas(640,480);
-    dog = createImg('images/dog.png',imageReady);
-    dog.hide();
+    video = createCapture(VIDEO);
+    //dog.hide();
+    video.hide();
     
     background(0);
-    mobilenet = ml5.imageClassifier('MobileNet',modelReady);
+    mobilenet = ml5.imageClassifier('MobileNet',video,modelReady);
 
+}
+function draw(){
+    image(video,0,0);
+    fill(0);
+    textSize(64);
+    text(label,10,height-100);
 }
